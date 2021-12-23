51bb6eaebb7ff6062352db974db555aee52aac56
bansal2203 <96591659+bansal2203@users.noreply.github.com>
2021-12-23 23:31:37 +0530

    Naman Bansal

---
+++ RigidBody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBody : MonoBehaviour
+{
+    [SerializeField] private float _forceamount = 100f;
+
+    private RigidBody rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<RigidBody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * _forceamount);
+        }
+    }
+}
